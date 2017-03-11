d2c4aa90c6045cdbbf7204158270dab7f9ccf50f
Gharpure <ygharpur@bmc.com>
2017-03-11 16:42:39 +0530

    mavenProject

---
+++ JenkinsGitSetUp/src/main/java/firstPackage/FirstClass.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+import org.testng.annotations.Test;
+
+public class FirstClass {
+
+	@Test
+	public void testMethod1(){
+		System.out.println("###############Successfull#####################");
+	}
+}
